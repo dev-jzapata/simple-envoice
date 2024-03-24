6c33bf2fa595885e088962b10524758e3e53929c
dev-jzapata <dev.jzapata@gmail.com>
2024-03-25 00:30:27 +0100

    Proyecto limpio y preconfigurado

---
+++ src/main/java/com/devjzapata/simpleenvoice/SimpleEnvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.devjzapata.simpleenvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEnvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEnvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjzapata/simpleenvoice/SimpleEnvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjzapata.simpleenvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEnvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
